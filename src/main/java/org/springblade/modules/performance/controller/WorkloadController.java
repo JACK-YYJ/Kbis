36b57314e69fc308f7381c5a30f9baef0a695a68
yyj <1171273901@qq.com>
2022-09-19 09:09:42 +0800

    工作量绩效校验 完成

--- src/main/java/org/springblade/modules/performance/controller/WorkloadController.java
+++ src/main/java/org/springblade/modules/performance/controller/WorkloadController.java
@@ -25,6 +25,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/kpi/work")
 @Api(tags = "工作量绩效分值")
+@CrossOrigin
 public class WorkloadController {
 	@Autowired
 	private KpiWorkloadService kpiWorkloadService;
